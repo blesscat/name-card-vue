017951db9cdbaebc838d443fc9e2d675b0198de4
blesscat <blesscat@gmail.com>
2018-09-04 20:31:00 +0800

    test

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'name-card-vue',
+    assetsPublicPath: '/name-card-vue',
 
     /**
      * Source Maps
